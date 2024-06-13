bafeb7878bd0db4dd2f198eb5992049db6b8304b
eudis <eudis.perdomo@pragma.com.co>
2024-06-13 10:32:30 -0500

    api con validador y envío a cola de mensajes

--- lib/apwsqslambda-stack.ts
+++ lib/apwsqslambda-stack.ts
@@ -1,16 +1,186 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { ApiGatewayToSqs, ApiGatewayToSqsProps } from "@aws-solutions-constructs/aws-apigateway-sqs";
+import * as api from 'aws-cdk-lib/aws-apigateway';
+import { OpenApiGatewayToLambda } from '@aws-solutions-constructs/aws-openapigateway-lambda';
+import { Asset } from 'aws-cdk-lib/aws-s3-assets';
+import * as path from 'path';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+import * as defaults from '@aws-solutions-constructs/core';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
 
 export class ApwsqslambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    var props2: ApiGatewayToSqsProps = {}
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApwsqslambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // Setup the queue
+    const buildQueueResponse = defaults.buildQueue(this, 'queue', {
+      existingQueueObj: props2.existingQueueObj,
+      queueProps: props2.queueProps,
+      deployDeadLetterQueue: props2.deployDeadLetterQueue,
+      deadLetterQueueProps: props2.deadLetterQueueProps,
+      maxReceiveCount: props2.maxReceiveCount,
+      enableEncryptionWithCustomerManagedKey: props2.enableEncryptionWithCustomerManagedKey,
+      encryptionKey: props2.encryptionKey,
+      encryptionKeyProps: props2.encryptionKeyProps
+    });
+    const sqsQueue = buildQueueResponse.queue;
+    const deadLetterQueue = buildQueueResponse.dlq;
+
+    const requestTemplates = {
+      "application/json": 'Action=SendMessage&MessageBody=$util.urlEncode(\"$input.body\")'
+    };
+
+
+    // Setup the API Gateway
+    const globalRestApiResponse = defaults.GlobalRestApi(this, {}, props2.logGroupProps);
+    const apiGateway = globalRestApiResponse.api;
+    const apiGatewayCloudWatchRole = globalRestApiResponse.role;
+    const apiGatewayLogGroup = globalRestApiResponse.logGroup;
+
+    // Setup the API Gateway role
+    const apiGatewayRole = new iam.Role(this, 'api-gateway-role', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com')
+    });
+
+
+    apiGatewayRole.addToPolicy(new iam.PolicyStatement({
+      resources: [
+        sqsQueue.queueArn
+      ],
+      actions: ["sqs:SendMessage"]
+    }));
+    // Use user-provided integration responses, otherwise fallback to the default ones we provide.
+    const integrationResponses = [
+      {
+        statusCode: "200"
+      },
+      {
+        statusCode: "500",
+        responseTemplates: {
+          "text/html": "Error"
+        },
+        selectionPattern: "500"
+      }
+    ];
+
+    let baseProps: apigateway.AwsIntegrationProps = {
+      service: 'sqs',
+      path: `${cdk.Aws.ACCOUNT_ID}/${sqsQueue.queueName}`,
+      integrationHttpMethod: "POST",
+      options: {
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+        credentialsRole: apiGatewayRole,
+        requestParameters: {
+          "integration.request.header.Content-Type": "'application/x-www-form-urlencoded'"
+        },
+        requestTemplates,
+        integrationResponses
+      }
+    };
+
+    var apiGatewayIntegration = new apigateway.AwsIntegration(baseProps);
+
+    const schema = {
+      type: apigateway.JsonSchemaType.OBJECT,
+      properties: {
+        data_insert: { type: apigateway.JsonSchemaType.STRING },
+        uuid: { type: apigateway.JsonSchemaType.STRING },
+        wallet_status: { type: apigateway.JsonSchemaType.STRING },
+        first_name: { type: apigateway.JsonSchemaType.STRING },
+        email: { type: apigateway.JsonSchemaType.STRING },
+        residence_country: { type: apigateway.JsonSchemaType.STRING },
+        nationality: { type: apigateway.JsonSchemaType.STRING },
+        card_status: { type: apigateway.JsonSchemaType.STRING },
+        account_creation_date: { type: apigateway.JsonSchemaType.STRING },
+        account_deletion_date: { type: apigateway.JsonSchemaType.STRING },
+        last_login_date: { type: apigateway.JsonSchemaType.STRING },
+        wallet_last_recharge: {
+          type: apigateway.JsonSchemaType.OBJECT,
+          properties: {
+            channel: { type: apigateway.JsonSchemaType.STRING },
+            date: { type: apigateway.JsonSchemaType.STRING }
+          },
+          required: ["channel", "date"]
+
+        },
+        card_last_recharge: {
+          type: apigateway.JsonSchemaType.OBJECT,
+          properties: {
+            channel: { type: apigateway.JsonSchemaType.STRING },
+            date: { type: apigateway.JsonSchemaType.STRING }
+          },
+          required: ["channel", "date"],
+
+        },
+        last_external_transfer_date: { type: apigateway.JsonSchemaType.STRING },
+        last_p2p_date: { type: apigateway.JsonSchemaType.STRING },
+        last_recharge_request_date: { type: apigateway.JsonSchemaType.STRING },
+        last_service_payment: {
+          type: apigateway.JsonSchemaType.OBJECT,
+          properties: {
+            date: { type: apigateway.JsonSchemaType.STRING },
+            service: { type: apigateway.JsonSchemaType.STRING }
+          },
+          required: ["service", "date"],
+
+        },
+        last_commerce_payment: {
+          type: apigateway.JsonSchemaType.OBJECT,
+          properties: {
+            commerce: { type: apigateway.JsonSchemaType.STRING },
+            date: { type: apigateway.JsonSchemaType.STRING },
+          },
+          required: ["commerce", "date"],
+
+        },
+        event: { type: apigateway.JsonSchemaType.STRING }
+      },
+      required: ["wallet_status", "uuid", "first_name", "email", "residence_country", "nationality", "card_status", "event"],
+    }
+    const requestModel = new apigateway.Model(this, 'RequestModel', {
+      restApi: apiGateway,
+      contentType: 'application/json',
+      schema: schema
+    });
+
+    const requestValidator = new apigateway.RequestValidator(this, 'RequestValidator', {
+      restApi: apiGateway,
+      requestValidatorName: 'MyRequestValidator',
+      validateRequestBody: true,
+      validateRequestParameters: false,
+    });
+
+    const defaultMethodOptions : apigateway.MethodOptions = {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Content-Type": true
+          }
+        },
+        {
+          statusCode: "500",
+          responseParameters: {
+            "method.response.header.Content-Type": true
+          },
+        }
+      ],
+      requestValidator: requestValidator,
+      requestModels: { 'application/json': requestModel }
+    };
+
+    // Setup the API Gateway method    
+    apiGateway.root.addMethod("POST", apiGatewayIntegration, defaultMethodOptions);
+
+
   }
 }
---
+++ sam-app/hello-world/jest.config.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    transform: {
+        '^.+\\.ts?$': 'ts-jest',
+    },
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    testMatch: ['**/tests/unit/*.test.ts'],
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiamVzdC5jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJqZXN0LmNvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7OztHQUdHOztBQUVILGtCQUFlO0lBQ1gsU0FBUyxFQUFFO1FBQ1AsWUFBWSxFQUFFLFNBQVM7S0FDMUI7SUFDRCxVQUFVLEVBQUUsSUFBSTtJQUNoQixlQUFlLEVBQUUsSUFBSTtJQUNyQixpQkFBaUIsRUFBRSxVQUFVO0lBQzdCLGdCQUFnQixFQUFFLElBQUk7SUFDdEIsU0FBUyxFQUFFLENBQUMseUJBQXlCLENBQUM7Q0FDekMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qXG4gKiBGb3IgYSBkZXRhaWxlZCBleHBsYW5hdGlvbiByZWdhcmRpbmcgZWFjaCBjb25maWd1cmF0aW9uIHByb3BlcnR5IGFuZCB0eXBlIGNoZWNrLCB2aXNpdDpcbiAqIGh0dHBzOi8vamVzdGpzLmlvL2RvY3MvY29uZmlndXJhdGlvblxuICovXG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgICB0cmFuc2Zvcm06IHtcbiAgICAgICAgJ14uK1xcXFwudHM/JCc6ICd0cy1qZXN0JyxcbiAgICB9LFxuICAgIGNsZWFyTW9ja3M6IHRydWUsXG4gICAgY29sbGVjdENvdmVyYWdlOiB0cnVlLFxuICAgIGNvdmVyYWdlRGlyZWN0b3J5OiAnY292ZXJhZ2UnLFxuICAgIGNvdmVyYWdlUHJvdmlkZXI6ICd2OCcsXG4gICAgdGVzdE1hdGNoOiBbJyoqL3Rlc3RzL3VuaXQvKi50ZXN0LnRzJ10sXG59O1xuIl19
